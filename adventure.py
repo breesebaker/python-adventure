34c76fd8270ba92c0bad77ddbe4adee5c969d33c
breesebaker <cbreesebaker@gmail.com>
2023-01-19 15:17:53 -0600

    Finnished script of game.

--- adventure.py
+++ adventure.py
@@ -1,88 +1,67 @@
+import os 
+from time import sleep 
+import logging 
 
+logging.basicConfig(filename="adventureLog.log", level=logging.DEBUG)
 
-
-def introduction():
-  print("|-----------------------------------------------------------------------------------------------------------|")  
-  print("|                                      Welcome to the Houston Food Selector!                                |") 
-  print("|-----------------------------------------------------------------------------------------------------------|") 
-  print("") 
-  print("") 
-  print("") 
-  print("") 
 def beggining():
-  start_adventure = input("Are you hungry?")
+  start_adventure = input("Hello Hungry Houstonian! Are you hungry? ")
   if start_adventure == "yes":
+    very_hungry()
+  elif start_adventure == "no":
+    print("Come back when your hungry!!")
 
-#loop back
-#not valid answer
+  very_hungry()
 
-    mid()  
+def very_hungry():
+  hunger_one = input("where would you like to go? Choose from the following options: Mexican, Indian, American, Southeast Asian ") 
+  if hunger_one == "mexican":
+    mexican_food = input("You chose Mexican, Good choice! Here are some of my favorite spots. Which one sounds good? Picos, Hugos or Ninfas ")
+    if mexican_food == input("Picos"):
+      print("Picos is my favorite authentic Mexican in town!")
+      print("They are located in the Upper Kirby area at 3601 Kirby Dr, Houston, Tx 77098")
+    elif mexican_food == input("Hugos"):
+      print("If your looking for authentic mexican food from an award winning chef, look no further!")
+      print("Located in Houstons' historic Montrose area, thier address is 1600 Westheimer, Houston, Tx 77006")
+    elif mexican_food == input("Ninfas"):
+      print("Welcome to the birthplace of fajitas! Ninfas is Houstons most popular Mexican restuarant in the city!")
+      print("Head on over to Houstons's East Downtown area to 2704 Navigation Blvd, Houston, Tx 77003")
+      #possible return
+  elif hunger_one == "indian":
+    indian_food = input("You chose Indian food! Theres nothing like vindaloo and fresh garlic na'an! Which one sounds good? Shivas, Aga's or Khyber ")
+    if indian_food == input("Shivas"):
+      print("If your looking for some quick and easy, authentic southern indian stye food Shivas is the place for you!")
+      print("Located in the West University area of Houston, Shiva offered home cooked, authentic delicious food. Thier address is 5220 Buffalo Speedway, Houston, Tx 77005")
+    elif indian_food == input("Agas"):
+      print("Aga's is by far my favorite place for anyhting Indo-Pak. They are a little ways away from where I live but oh so worht the drive!")
+      print("If your looking for a more upscale authentic Indian food experience for special occasions. They are located at 11842 Wilcrest Dr, Houston, Tx, 77031")
+    elif indian_food == input("Khyber"):
+      print("Some of my favorite northen Indian food in town. Khyber offers some of the best butter chicken and vindaloos!")
+      print("Head over to Houston's Upper Kirby neighboorhood to try them out! Their address is 2510 Richmond Ave, Houston, Tx, 77098")      
+      #possible return
+  elif hunger_one == "american":
+    american_food == input("You chose American food! Out of my most frequently visited, which one sounds good? Graces, Empire Cafe or Truth BBQ ")
+    if american_food == input("Graces"):
+      print("When it comes to some american southern food, Grace's is by far one of the best in town. Try the Rosies fried chicken!")
+      print("Another gem hidden in Houstons' Upper Kirby neighborhood, you can head on over to 3111 Kirby Dr, Houston, Tx 77098")
+    elif american_food == input("Empire Cafe"):
+      print("A Houston staple! Empire Cafe is a place everyone needs when in search of oversized breakfast plates and unlimited amount of fresh coffee!")
+      print("Empire is located in Houstons historical Montrose district in what used to be a gas station. Thier address is 1732 Westheimer Rd, Houston, Tx 77098")
+    elif american_food == input("Truth BBQ"):
+      print("hailed as one of the best BBQ spots in the state, Truth's brisket alone is worth standing in line for.")
+      print("Truth is located in the Heights neighboor hood just south of I-10 at 110 S Heights Blvd, Houston, Tx, 77007")
+      #possible return
 
-def mid(): 
-  first_choice = input("Your at the drivethrough of a McDonalds. You are very hungry and want to get your usual order, a 30 pack of chicken nuggets and get back home and enjoy them. But you see that the McRib is back. Which do you choose? (McRib/Nuggets)")   
-  if first_choice == "mcrib":
-    secound_choice = input("You order a McRib but are considering getting two because they are a limited time thing. Do you get one or two?")
-    if secound_choice == "two":
-      print("That second McRib was a bad choice and you get food poisoning")
-    else:
-      print("You are home enjoying your single McRib. But now your craving something sweet... TO BE CONTINUED")
-
-  elif first_choice == "nuggets":
-    print("You made the right choice and are happily on your way home to enjoy you snack")
-
-  else:
-    print("Come back when your ready!")
-
-beggining() 
-
-# story script:
-# general idea - pull form a book(LOTR)/video game/random adventure based in houston/scary story?
-
-
-Are you hungry? (Yes/No)
-
-  Yes?
-    Where would you like to go? (Mexican, Indian, American, Thai, Vietnamese)
-
-    Mexican
-      Where would you like to go? (Picos, Little Papasitos, Ninfas)
-        Picos (Address, Phone Number)
-        Little Papasitos (Address, Phone Number)
-        Ninfas (Address, Phone Number)
-      Return
-
-    Idian
-      Where would you like to go? (Shivas, Khyber, Kirans, Agas)
-        Shivas (Address, Phone Number)
-        Khyber Papasitos (Address, Phone Number)
-        Kirans (Address, Phone Number)
-        Agas (Address, Phone Number)
-      Return
-
-    American
-      Where would you like to go? (Graces, Moxies, Mias Table, Roost)
-        Graces (Address, Phone Number)
-        Moxies (Address, Phone Number)
-        Mias Table (Address, Phone Number)
-        Roost (Address, Phone Number)
-      Return
-
-    Thai
-      Where would you like to go? (Rim Tanon, Thai Bistro, Street Food Thai Market, Kin Dee)
-        Rim Tanon (Address, Phone Number)
-        Thai Bistro (Address, Phone Number)
-        Street Food Thai Market (Address, Phone Number)
-        Kin Dee (Address, Phone Number)
-      Return
-
-    Vietnamese
-      Where would you like to go? (Lua Viet, Pho Siagon, Kua Ba, Fat Bao)
-        Lua Viet (Address, Phone Number)
-        Pho Siagon (Address, Phone Number)
-        Kua Ba (Address, Phone Number)
-        Fat Bao (Address, Phone Number)
-      Return
-
-
-No?
-  Come back when you are hungry!
+  elif hunger_one == "southeast asian":
+    thai_food = input("You chose Southeast Asian Food! Which of these amazing places from thailand to vietnam would you like to try? (Rim Tanon, Kin Dee, Lua Viet ")
+    if thai_food == input("Rim Tanon"):
+      print("Want all the street food that Thailand has to offer without the jetlag? Look no further than Rim Tanon!")
+      print("Rim Tanon bring Thai street food to your own backyard! Check them out at 2241 Richmond Ave, Houston, Tx, 77098")
+    elif thai_food == input("Kin Dee"):
+      print("Kin Dee offers some of the best homecooked thai food in Houston. Chef Miranda incorporates some of her favorite dishes from her home in Thailand")
+      print("Head of to the Houston Height just south of the north loop to Kin Dee. Their address is 1533 N Shepherd Dr, Houston, Tx 77008")
+    elif thai_food == input("Lua Viet"):
+      print("My absolue favorite spot for Vietnamsese food! Lua Viet offers locally sourced ingredients and a welcome atmosphere for any occasion!")
+      print("Lua Viet can be found in Houstons Montrose district at 1540 W Alabama St, Houston, Tx 77006 ")  
+    
+beggining()
